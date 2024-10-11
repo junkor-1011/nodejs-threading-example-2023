4956a393a303ec94fec6815b663a854d98d79b40
junkor-1011 <tomoyukizxy@gmail.com>
2024-10-11 22:46:54 +0900

    commander

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.debug("hello");
+import { isMainThread } from "node:worker_threads";
+
+import { Option, program } from "commander";
+
+if (isMainThread) {
+  program
+    .name("example-app")
+    .description("app with threading")
+    .addOption(
+      new Option("-n --number <number", "number of jobs").default(100)
+        .argParser(parseFloat),
+    );
+  program.parse();
+
+  const opts = program.opts(); // todo: schema validation
+  console.debug(opts);
+} else {
+  console.debug("[threading] todo: implementation");
+}
